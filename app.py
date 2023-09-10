6a63c74c978b54e73d802b549a2730d15fdbfd99
sooraj <soorajks2002@gmail.com>
2023-09-10 18:43:11 +0530

    Streamlit app

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 import streamlit as st
 from remove_bg import remove_bg
-from generate_image import generate_image
+from generate_bg import generate_image
 from superimpose import superimpose_image
 
-st.title("Background Brush")
+st.title("BG Brush 🧑‍🎨")
 
-input_image = st.file_uploader("Upload your target image", type=['png', 'jpg', 'jpeg'], label_visibility='hidden')
+st.markdown("#### Select Your Image")
+input_image = st.file_uploader("Upload your target image", type=['png', 'jpg', 'jpeg'], label_visibility='collapsed')
 if input_image :
-    segmented_image = remove_bg(input_image)
-
-    st.image(segmented_image)
-    
-    prompt_input = st.text_input("Please Enter Prompt for new Background")
-    
-    if prompt_input :
-        with st.spinner("Generating Background") :
-            new_bg = generate_image(prompt_input)
-        if new_bg['image_generated'] :
-            st.image(new_bg['image'])
-            with st.spinner("Superimposing Background") :
-                st.image(superimpose_image(segmented_image, new_bg['image']))
+    st.markdown("#### Enter background description")
+    prompt_input = st.text_input("Please Enter Prompt for new Background", label_visibility='collapsed')
+    if st.button("Get New Background") :
+        if prompt_input :
+            with st.spinner("Generating Background") :
+                new_bg = generate_image(prompt_input)
+                
+            if new_bg['image_generated'] :
+                with st.spinner("Removing background") :
+                    segmented_image = remove_bg(input_image)
+                 
+                img1, img2 = st.columns(2)
+                with img1 :
+                    st.markdown("#### Segmentated Image")
+                    st.image(segmented_image)
+                    
+                with img2 :   
+                    with st.spinner("Superimposing Background") :
+                        st.markdown("#### New Image")
+                        st.image(superimpose_image(segmented_image, new_bg['image']))
+            else :
+                st.info("API BUSY!")
+            
         else :
-            st.info("API BUSY!")
+            st.warning("Please Enter The Prompt")
